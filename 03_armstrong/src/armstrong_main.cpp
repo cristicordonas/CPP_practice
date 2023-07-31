26aa132eda4273718a042a697e6a5a9b0f3089a7
Cordonas Cristian <cordonascristian@gmail.com>
2023-07-31 14:37:54 +0300

    03 solved

--- 03_armstrong/src/armstrong_main.cpp
+++ 03_armstrong/src/armstrong_main.cpp
@@ -2,8 +2,19 @@
 
 bool isArmstrongNumber(int number)
 {
-	// TODO: implement some functionality to see if this number is an armstrong number
-
+	int originalNum, digit, result;
+	originalNum = number;
+	result = 0;
+	while (number != 0) 
+	{
+		digit = number % 10;
+		result += digit * digit * digit;
+		number /= 10;
+	}
+	if (result == originalNum)
+	{
+		return true;
+	}
 	return false;
 }
 
@@ -21,26 +32,7 @@ void printIsArmstrong(int number)
 
 int main(int argc, char *argv[])
 {
-	// What is this program expected to do?
-	// - Shows whether an argument is an armstrong number.
-	// (what?)
-	// -	An Armstrong number is a number that is equal to the sum of cubes of its digits.
-	//		For example 0, 1, 153, 370, 371 and 407 are the Armstrong numbers.
-	//		Let's try to understand why 153 is an Armstrong number:
-	//			1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
-	//
-	// How to launch it?
-	// - Execute the binary and pass a parameter to it?
-	// - E.g. Open CMD in bin/Debug or bin/Release
-	//		  03_armstrong.exe 1		=> Output: Armstrong
-	//		  03_armstrong.exe 2		=> Output: NOT Armstrong
-	//		  03_armstrong.exe 			=> Output: No program arguments found.
-	//		  03_armstrong.exe ABC		=> Undefined output (do whatever).
-	//		  03_armstrong.exe 153		=> Output: Armstrong
-	//		  03_armstrong.exe 154		=> Output: NOT Armstrong
-	//
 
-	// Make sure there are some program arguments available.
 	if (argc <= 1)
 	{
 		std::cout << "No program arguments found." << std::endl;
@@ -48,10 +40,14 @@ int main(int argc, char *argv[])
 	}
 
 	int readNumber = 0;
-	// Get the first argument
 	std::string argumentAsString = argv[1];
 	
-	// TODO: read number / cast to integer
+	readNumber = std::atoi (argumentAsString.c_str());
+	if (readNumber == 0 && argumentAsString != "0")
+	{
+		printf("NAN\n");
+		return 1;
+	}
 
 	printIsArmstrong(readNumber);
 	return 0;
