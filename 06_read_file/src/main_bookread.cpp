d1c8582be9dd1cedaa52a0dc11f203f02f0b44bd
Cordonas Cristian <cordonascristian@gmail.com>
2023-08-02 20:35:56 +0300

    Tasks completed for project 06

--- 06_read_file/src/main_bookread.cpp
+++ 06_read_file/src/main_bookread.cpp
@@ -1,10 +1,9 @@
 ﻿#include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 
-/**
-	Define a simple book.
-*/
+
 class Book
 {
 public:
@@ -18,45 +17,46 @@ public:
 	}
 };
 
-/**
-	Reads a vector of books from a text file.
-	Note: the text file shall contain lines of text in pairs for the book name and authors
-	E.g.
-		BOOK 1
-		AUTHOR 1
-		BOOK 2
-		AUTHOR 2
-		BOOK 3
-		AUTHOR 1, AUTHOR 2, AUTHOR 3
-	@param file_name The name of the file to read from (must include path).
-	@return Vector of books.
-*/
+
 std::vector<Book> readBooksFromTextFile(const std::string& file_name)
 {
 	std::vector<Book> results;
-	// TODO: BEGIN read the file -------------------------------------
+	std::ifstream file(file_name);
+	if (file.is_open())
+	{
+		std::string line;
+		int line_number = 0;
+		Book myBook;
+		while (std::getline(file, line))
+		{
+			line_number++;
+			if (line_number % 2 == 1)
+			{
+				myBook.name = line;
+			}
+			else
+			{
+				myBook.authors = line;
+				results.emplace_back(myBook);
+			}
 
+		}
+		file.close();
+	}
+	else
+	{
+		std::cout << "The file cannot be opened." << std::endl;
+	}
 
-	// E.g. Book myBook;
-	//		...
-	//		results.emplace_back(myBook);
-
-	// TODO: END read file and add to results vector ------------------
 	return results;
 }
 
 int main()
 {
-	// Read a collection of books from a file.
-	// Simple examples can be found everywhere.
-	// E.g. https://stackoverflow.com/questions/7868936/read-file-line-by-line-using-ifstream-in-c
-
-	// Read the data
 	std::string input_data("../../data/ermahgerd_berks.txt");
 	std::cout << "Reading the data from " << input_data << std::endl;
 	std::vector<Book> books_from_file = readBooksFromTextFile(input_data);
 
-	// Print the data
 	std::cout << "Here are all the books found in the data file..." << std::endl;
 	for (auto book : books_from_file)
 	{
