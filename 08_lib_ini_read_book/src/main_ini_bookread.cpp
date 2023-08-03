2690220bd9fd0e2db654732b6940109f8cbe5faa
Cordonas Cristian <cordonascristian@gmail.com>
2023-08-03 11:41:28 +0300

    Tasks completed for project 08

--- 08_lib_ini_read_book/src/main_ini_bookread.cpp
+++ 08_lib_ini_read_book/src/main_ini_bookread.cpp
@@ -6,12 +6,8 @@
 #include <string>
 #include <vector>
 
-// 3rd party libs headers
 #include <SimpleIni.h>
 
-/**
-	Define a simple book.
-*/
 class Book
 {
 public:
@@ -25,54 +21,43 @@ public:
 	}
 };
 
-/**
-	Reads a vector of books from an INI file.
-	Note: the INI file shall contain a general books section with the number of books contained,
-	and another INI section for each book, named [book.N], where N is a number/counter
-
-	[books]
-	count = 2;
-
-	[book.1]
-	name=The origin of truth
-	author=Gusti
-	[book.2]
-	name  = Arhanghelul Raul
-	author=Ovidiu Eftimie
-
-	@param file_name The name of the file to read from (must include path).
-	@return Vector of books.
-*/
 std::vector<Book> readBooksFromIniFile(const std::string& file_name)
 {
 	std::vector<Book> results;
-	// TODO: BEGIN read the file -------------------------------------
-	
-	// E.g. Book myBook;
-	//		// build the section name (E.g. book.1)
-	//		std::stringstream ss;
-	//		ss << "book." << (i + 1);
-	//		// Copy the stream to a string you can use
-	//		std::string section_name(ss.str());
+	CSimpleIniA ini;
+	ini.SetUnicode();
+	SI_Error rc = ini.LoadFile(file_name.c_str());
+	if (rc < 0) 
+	{
+		std::cout << "The file can not be accessed" << std::endl;
+		return results;
+	}
 
-	//		...
-	//		results.emplace_back(myBook);
-
-	// TODO: END read file and add to results vector ------------------
+	Book myBook;
+	std::string section_name;
+	std::string section_authors;
+	const char* count;
+	count = ini.GetValue("books", "count");
+	std::stringstream ss;
+	for (int i = 0; i < std::atoi(count); i++)
+	{
+		ss.str("");
+		ss << "book." << (i + 1);
+		section_name = ini.GetValue(ss.str().c_str(), "name");
+		section_authors = ini.GetValue(ss.str().c_str(), "author");
+		myBook.name = section_name;
+		myBook.authors = section_authors;
+		results.emplace_back(myBook);
+	}
 	return results;
 }
 
 int main()
 {
-	// Read a collection of books from an INI file.
-	// Using the SimpleINI C++ Lib: https://github.com/brofield/simpleini
-
-	// Read the data
-	std::string input_data("PATH_TO_INI_FILE.ini");
+	std::string input_data("../../data/ermahgerd_berks.ini");
 	std::cout << "Reading the data from " << input_data << std::endl;
 	std::vector<Book> books_from_file = readBooksFromIniFile(input_data);
 
-	// Print the data
 	std::cout << "Here are all the books found in the data file..." << std::endl;
 	for (auto book : books_from_file)
 	{
